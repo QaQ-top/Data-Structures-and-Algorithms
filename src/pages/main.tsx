3a4c8c8a5fc77d414d99f3d754a034566d534a75
ck <2941498354@qq.com>
2021-05-02 20:37:15 +0800

    添加 最大堆 最小堆

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,6 +3,10 @@ import { createApp } from '@vue/runtime-dom';
 import './style.sass';
 const routes = [
   {
+    name: '参考学习',
+    path: 'https://github.com/trekhleb/javascript-algorithms/blob/83357075c4698f487af733e6e0bf9567ba94c266/src/data-structures'
+  },
+  {
     name: '链表',
     path: 'linked-list',
     description: '重点: 如何删除某个节点，如何反转整个链表'
@@ -42,7 +46,7 @@ const Router = {
           return <li class="nav_item" {...{key: i.path}} > {/* 如果出现 attrs="[Object Object]" 使用扩展运算符添加属性 */}
             <a {...{href: i.path}}>{i.name}</a>
             &nbsp;&nbsp;&nbsp;
-            <span style={{fontSize: '10px', color: 'red'}}>{i.description}</span>
+            <span style={{fontSize: '10px', color: 'red'}}>{i?.description}</span>
             <br />
             <br />
           </li>
